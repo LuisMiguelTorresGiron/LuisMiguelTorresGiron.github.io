e1fa960ed58ef5a84adcdbf68b3cec278f184160
Miguel <torres_119@hotmail.con>
2021-05-22 19:39:07 -0500

    h1

---
+++ Funciones.js
@@ -0,0 +1,10 @@
+// Mensaje de Bienv
+
+function Mostrar() {
+    var miDiv = document.getElementById
+}
+
+document.getElementById('mensaje').innerHTML='mensaje'
+
+document.write("<div style='background-color:#776199;'>Bienvenido</div>")
+
